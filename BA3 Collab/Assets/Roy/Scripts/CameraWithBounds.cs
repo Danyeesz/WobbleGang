8f2704261d5d80b7c8fcea5cba8c545d152cd339
RoyDevashis <roydeitz@gmail.com>
2022-01-11 14:44:32 +0100

    Camera Change logic

--- BA3 Collab/Assets/Roy/Scripts/CameraWithBounds.cs
+++ BA3 Collab/Assets/Roy/Scripts/CameraWithBounds.cs
@@ -40,9 +40,9 @@ public class CameraWithBounds : MonoBehaviour
     private void Move()
     {
         Vector3 centerPoint = GetCenterPosition();
-        //Vector3 newPosition = centerPoint + offset;
-        Vector3 newCenterPos = new Vector3(transform.position.x, centerPoint.y, centerPoint.z);
-        Vector3 newPosition = newCenterPos + offset;
+        Vector3 newPosition = centerPoint + offset;
+        //Vector3 newCenterPos = new Vector3(transform.position.x, centerPoint.y, centerPoint.z);
+        //Vector3 newPosition = newCenterPos + offset;
         transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
 
