2d9fcfc735d25a38dbed3e5d7130cee4120c2d33
RoyDevashis <roydeitz@gmail.com>
2022-01-14 14:43:29 +0100

    GroundTile Auto generate without Holes

--- BA3 Collab/Assets/Roy/Scripts/AutoGroundHoleGenerator.cs
+++ BA3 Collab/Assets/Roy/Scripts/AutoGroundHoleGenerator.cs
@@ -10,8 +10,10 @@ public class AutoGroundHoleGenerator : MonoBehaviour
     public Transform[,] positions2D;
     public int rowSize;
     public int columnSize;
-    public float groundLength=1;
+    public float groundLength=2;
     public Vector3 startingPosition;
+    private Vector3 curentPosition;
+    private float groundHeight;
 
 
     public float timeTillNextChange;
@@ -20,7 +22,10 @@ public class AutoGroundHoleGenerator : MonoBehaviour
     {
         Transform[,] array2D = new Transform[rowSize,columnSize];
         positions2D = array2D;
-        
+        groundHeight = startingPosition.y;
+        curentPosition = startingPosition;
+
+        ArrangeTiles();
     }
 
     // Update is called once per frame
@@ -31,6 +36,33 @@ public class AutoGroundHoleGenerator : MonoBehaviour
 
     void ArrangeTiles() 
     {
-    
+        for (int row = 0; row < rowSize; row++) 
+        {
+            for (int column = 0; column < columnSize; column++)
+            {
+                Instantiate(groundTile, curentPosition, groundTile.transform.rotation);
+                if (column == columnSize - 1)
+                {
+                    curentPosition = new Vector3(startingPosition.x, groundHeight, curentPosition.z);
+                }
+                else
+                {
+                    float x = curentPosition.x + groundLength;
+                    curentPosition = new Vector3(x, groundHeight, curentPosition.z);
+                }
+            }
+            float z = curentPosition.z - groundLength;
+            curentPosition = new Vector3(curentPosition.x, groundHeight,z);
+        }
+
     }
+
+    void GenerateHoles() 
+    {
+        for (int i = 0; i < maxNoOfHoles; i++) 
+        {
+        
+        }
+    }
+   
 }
